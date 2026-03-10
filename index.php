d24bcfe010905ef32bd0ef00c474f2b3b1c0c7c1
murphy-afk <bardarenoemi@gmail.com>
2026-03-10 12:28:32 +0100

    feat: add new record

--- index.php
+++ index.php
@@ -35,22 +35,22 @@ require_once 'server.php';
       ?>
       <h2 class="mt-4 mb-3">Add a record</h2>
       <div class="card p-4 shadow-sm">
-        <form action="server.php" method="POST" class="row g-3">
+        <form action="index.php" method="POST" class="row g-3">
           <div class="col-12">
             <label for="title" class="form-label">Title</label>
-            <input type="text" id="title" name="title" class="form-control" >
+            <input type="text" id="title" name="title" class="form-control" required>
           </div>
           <div class="col-12">
             <label for="artist" class="form-label">Artist</label>
-            <input type="text" id="artist" name="artist" class="form-control" >
+            <input type="text" id="artist" name="artist" class="form-control" required>
           </div>
           <div class="col-12">
             <label for="genre" class="form-label">Genre</label>
-            <input type="text" id="genre" name="genre" class="form-control" >
+            <input type="text" id="genre" name="genre" class="form-control" required>
           </div>
           <div class="col-12">
             <label for="year" class="form-label">Year</label>
-            <input type="number" name="year" id="year" max="2026" min="1860" class="form-control" >
+            <input type="number" name="year" id="year" max="2026" min="1860" class="form-control" required>
           </div>
           <div class="col-12">
             <button class="btn btn-primary w-100">Add</button>
--- server.php
+++ server.php
@@ -4,9 +4,16 @@ $records_text = file_get_contents('records.json');
 
 $records = json_decode($records_text, true);
 
-if (isset($_POST['title'])) {
-  echo $_POST['title'];
+if (isset($_POST['title']) && isset($_POST['artist']) && isset($_POST['genre']) && isset($_POST['year'])) {
+  $randomImage = "https://picsum.photos/300?random=" . rand(1000, 9999);
+  $newRecord = [
+    "title" => $_POST['title'],
+    "artist" => $_POST['artist'],
+    "genre" => $_POST['genre'],
+    "year" => $_POST['year'],
+    "cover_url" => $randomImage
+  ];
+  $records[] = $newRecord;
+  file_put_contents('records.json', json_encode($records));
 }
-
-
 ?>
